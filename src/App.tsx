a06c6b7721979c234a2606f132e3ad49cb7ce0b3
JoakinGC <joakingcontreras2@gmail.com>
2025-02-10 22:29:32 +0100

    add base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-
 import './App.css'
 import ImageClassifier from './componets/ImageClassifier'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
